0e9b7cf964b7743523ce22e54dca34ea6ffc7d75
artermi <ericcheng230@gmail.com>
2021-07-28 14:37:17 +0800

    new things

---
+++ Task1_DP55/class_punisher.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+#include <cstring>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+#define K 0.5
+#define L 200
+#define LL (L * L)
+
+class punPGG{
+public:
+	long long rnd;
+	double r;
+	double beta;
+	double Dr; //Defactor's rate
+	double Pr; //Punisher rate
+	double T;
+	double Gp;
+	char dir_name[100];
+
+	int Strategy[LL]; // 0 for Defactor, 1 for Cooperator, 2 for Punisher
+	int Neighbour[LL][4];
+	//int MST_N[LL];
+	//int Next_MST_N[LL];
+	//double probs[LL];
+	//long long curr[LL];
+
+	punPGG(const double pr,const double dr, const double rate, const double gp,
+	 const double t,const double Beta); // rate of Pubisher, rate of Defactor, r,
+	double unit_game(const int cent,const int to);
+	double centre_game(const int cent, const int isX);
+	int game(bool ptf);
+};
---
+++ Task1_DP55/onBG.cpp
@@ -0,0 +1,16 @@
+#include "class_punisher.h"
+using namespace std;
+
+int main(){
+	srand(time(NULL));
+
+	for(int k = 0; k < 21; k++){
+		for(int j = 0; j < 21; j++){
+				double var_b = (double) k * 0.05;
+				double var_g = (double) j * 0.05; 
+				punPGG punOBJ(0.333333,0.333333,3.0,var_g,0.1,var_b);
+				punOBJ.game(true);	}	
+	}
+
+	return 0;
+}
