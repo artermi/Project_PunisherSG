b4565020a7baf890905eb6b5349084a53d440458
鄭格承 <colin@vmR189.ios.sinica.edu.tw>
2021-08-30 12:58:51 +0800

    merge

--- Task4/G_var.cpp
+++ Task4/G_var.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
 	srand(time(NULL));
 
-	for(double k = 0.87; k < 0.885; k+=0.01){
-		punPGG punOBJ(0.333333,0.333333,3.0,k,0.2,0.2);
+	for(double k = 0.47; k < 0.505; k+=0.01){
+		punPGG punOBJ(0.333333,0.333333,3.0,k,0,0.4);
 		punOBJ.game(true);		
 	}
 
